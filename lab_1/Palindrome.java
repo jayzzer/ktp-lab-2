5c8ea3760e6e0190de385aff0341104d0d7d9b97
Jayzzer <pavel10_pushkarev@mail.ru>
2018-02-19 12:05:02 +0300

    done lab_1

---
+++ lab_1/Palindrome.java
@@ -0,0 +1,23 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String s = args[i];
+
+            System.out.println(s + ": " + isPalindrome(s));
+        }
+    }
+
+    public static String reverseString(String s) {
+        String reverse = "";
+
+        for (int i = s.length()-1; i >= 0; i--) {
+            reverse += s.charAt(i);
+        }
+
+        return reverse;
+    }
+
+    public static boolean isPalindrome(String s) {
+        return s.equals(reverseString(s));
+    }
+}
